2eedd457bc208d795e9436ca5aaccec4a5d39628
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-16 21:51:10 -0300

    apostila sobre javascript
    bloco de codigo 1.1.2 encerrado

---
+++ script.js
@@ -0,0 +1,46 @@
+// var linguagem = "Javascript"
+// console.log("Aprendendo " + linguagem)
+// //linguagem pode ser redeclarada
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+
+//escopo não restrito a bloco
+var idade = 18
+console.log("Oi, " + nome)
+if (idade >= 18){
+    var nome = "João"
+    console.log("Parabéns, " + nome + ". Você pode dirigir.")
+}
+console.log("Até mais, " + nome + ".")
+
+
+
+
+
+
+// //declarando constantes
+// const nome = "José"
+// const idade = 27
+// /*comentário
+// de múltiplas linhas
+// */
+// const sexo = 'Masculino'
+// const endereco = `Rua K, 12`
+// console.log(nome, idade, sexo, endereco)
+
+// //let: declara variáveis
+// let a = 2
+// let b = 3
+// console.log(a, b)
+// a = a + b
+// console.log(a)
+
+// a = "SD & Mobile"
+// console.log(a)
+
+// a = true
+// console.log(a)
+
+// var unidadeCurricular = "DesWeb"
+// var deMaior = false
+// console.log(unidadeCurricular, deMaior)
